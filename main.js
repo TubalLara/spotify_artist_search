d5626eeb2626925cc2929cb8fd87d7fb6a765ebd
Tubal Lara <tublargar@gmail.com>
2015-08-04 19:16:32 +0200

    second iteration done

---
+++ main.js
@@ -0,0 +1,571 @@
+{
+  "artists" : {
+    "href" : "https://api.spotify.com/v1/search?query=iron&offset=0&limit=20&type=artist",
+    "items" : [ {
+      "external_urls" : {
+        "spotify" : "https://open.spotify.com/artist/6mdiAmATAx73kdxrNrnlao"
+      },
+      "followers" : {
+        "href" : null,
+        "total" : 736663
+      },
+      "genres" : [ "metal", "nwobhm" ],
+      "href" : "https://api.spotify.com/v1/artists/6mdiAmATAx73kdxrNrnlao",
+      "id" : "6mdiAmATAx73kdxrNrnlao",
+      "images" : [ {
+        "height" : 1202,
+        "url" : "https://i.scdn.co/image/98226e1830a09668c1b810bc90b4ad00ab6ef4f1",
+        "width" : 838
+      }, {
+        "height" : 918,
+        "url" : "https://i.scdn.co/image/ee4a9256c2e256615aa1eae7fd90052c9a760ab4",
+        "width" : 640
+      }, {
+        "height" : 287,
+        "url" : "https://i.scdn.co/image/315440239aa3669fbe3fded5f352b2f73b020d98",
+        "width" : 200
+      }, {
+        "height" : 92,
+        "url" : "https://i.scdn.co/image/bd1d0d4a7504a4c945707ddd7ecd334c53ba703c",
+        "width" : 64
+      } ],
+      "name" : "Iron Maiden",
+      "popularity" : 78,
+      "type" : "artist",
+      "uri" : "spotify:artist:6mdiAmATAx73kdxrNrnlao"
+    }, {
+      "external_urls" : {
+        "spotify" : "https://open.spotify.com/artist/4M5nCE77Qaxayuhp3fVn4V"
+      },
+      "followers" : {
+        "href" : null,
+        "total" : 312077
+      },
+      "genres" : [ "indie folk", "stomp and holler" ],
+      "href" : "https://api.spotify.com/v1/artists/4M5nCE77Qaxayuhp3fVn4V",
+      "id" : "4M5nCE77Qaxayuhp3fVn4V",
+      "images" : [ {
+        "height" : 667,
+        "url" : "https://i.scdn.co/image/981c27ce4d3c421d63e96d9e4b9ab006f544b478",
+        "width" : 1000
+      }, {
+        "height" : 427,
+        "url" : "https://i.scdn.co/image/47d2300f11d415a3dbc33e565970198a8abc819c",
+        "width" : 640
+      }, {
+        "height" : 133,
+        "url" : "https://i.scdn.co/image/ceb41cfc1aab2bc2b30b0fd51dab412fcf30504b",
+        "width" : 200
+      }, {
+        "height" : 43,
+        "url" : "https://i.scdn.co/image/8010395a8814ec4c5e70f5692235d592e813f278",
+        "width" : 64
+      } ],
+      "name" : "Iron & Wine",
+      "popularity" : 77,
+      "type" : "artist",
+      "uri" : "spotify:artist:4M5nCE77Qaxayuhp3fVn4V"
+    }, {
+      "external_urls" : {
+        "spotify" : "https://open.spotify.com/artist/3TocIPkVkpTWkKVkYSajgx"
+      },
+      "followers" : {
+        "href" : null,
+        "total" : 205
+      },
+      "genres" : [ ],
+      "href" : "https://api.spotify.com/v1/artists/3TocIPkVkpTWkKVkYSajgx",
+      "id" : "3TocIPkVkpTWkKVkYSajgx",
+      "images" : [ {
+        "height" : 640,
+        "url" : "https://i.scdn.co/image/c3b62141d26cc7cd6468657c0b434832c60ac2b6",
+        "width" : 640
+      }, {
+        "height" : 300,
+        "url" : "https://i.scdn.co/image/47e12616ea4eca59b7b2deb63f7110494c800998",
+        "width" : 300
+      }, {
+        "height" : 64,
+        "url" : "https://i.scdn.co/image/eabc9e36f9ebdaf5f6b1b49aefbc93211248b798",
+        "width" : 64
+      } ],
+      "name" : "Iron",
+      "popularity" : 66,
+      "type" : "artist",
+      "uri" : "spotify:artist:3TocIPkVkpTWkKVkYSajgx"
+    }, {
+      "external_urls" : {
+        "spotify" : "https://open.spotify.com/artist/6COmyouHXwCeIGS1IFd1PA"
+      },
+      "followers" : {
+        "href" : null,
+        "total" : 6140
+      },
+      "genres" : [ "german metal", "power metal" ],
+      "href" : "https://api.spotify.com/v1/artists/6COmyouHXwCeIGS1IFd1PA",
+      "id" : "6COmyouHXwCeIGS1IFd1PA",
+      "images" : [ {
+        "height" : 640,
+        "url" : "https://i.scdn.co/image/3cd8ef6e096e8bffbb24dce9e9be2983d357bd34",
+        "width" : 640
+      }, {
+        "height" : 300,
+        "url" : "https://i.scdn.co/image/49b8a85d7df2888073e883866f1fa82399b5b19c",
+        "width" : 300
+      }, {
+        "height" : 64,
+        "url" : "https://i.scdn.co/image/22ada88e24be5fda8aca838163b28402430a7d99",
+        "width" : 64
+      } ],
+      "name" : "Iron Savior",
+      "popularity" : 43,
+      "type" : "artist",
+      "uri" : "spotify:artist:6COmyouHXwCeIGS1IFd1PA"
+    }, {
+      "external_urls" : {
+        "spotify" : "https://open.spotify.com/artist/1v3CnxHAtLMfZW8v5dGj7t"
+      },
+      "followers" : {
+        "href" : null,
+        "total" : 11424
+      },
+      "genres" : [ ],
+      "href" : "https://api.spotify.com/v1/artists/1v3CnxHAtLMfZW8v5dGj7t",
+      "id" : "1v3CnxHAtLMfZW8v5dGj7t",
+      "images" : [ {
+        "height" : 640,
+        "url" : "https://i.scdn.co/image/40c66b25617f436c2e1ad6d84ec8af8ff7f8d6b8",
+        "width" : 640
+      }, {
+        "height" : 300,
+        "url" : "https://i.scdn.co/image/15504e69ad8107bd6f792e83cd5d074d00c4371c",
+        "width" : 300
+      }, {
+        "height" : 64,
+        "url" : "https://i.scdn.co/image/d8be322f0cbf03ce1c2a1ab8845a00971e4da137",
+        "width" : 64
+      } ],
+      "name" : "Iron Horse",
+      "popularity" : 47,
+      "type" : "artist",
+      "uri" : "spotify:artist:1v3CnxHAtLMfZW8v5dGj7t"
+    }, {
+      "external_urls" : {
+        "spotify" : "https://open.spotify.com/artist/3cdvlbCYf4WoBdy89RWkNS"
+      },
+      "followers" : {
+        "href" : null,
+        "total" : 19879
+      },
+      "genres" : [ "psychedelic rock" ],
+      "href" : "https://api.spotify.com/v1/artists/3cdvlbCYf4WoBdy89RWkNS",
+      "id" : "3cdvlbCYf4WoBdy89RWkNS",
+      "images" : [ {
+        "height" : 777,
+        "url" : "https://i.scdn.co/image/53d071d56b2d35fc7f22bdba34afb6210774f2f4",
+        "width" : 627
+      }, {
+        "height" : 248,
+        "url" : "https://i.scdn.co/image/c32eba2d6112311d02f41e5f300684c80ab1b6ce",
+        "width" : 200
+      }, {
+        "height" : 79,
+        "url" : "https://i.scdn.co/image/efc5dd002af719a94ff3ea483284aa4310df0c03",
+        "width" : 64
+      } ],
+      "name" : "Iron Butterfly",
+      "popularity" : 58,
+      "type" : "artist",
+      "uri" : "spotify:artist:3cdvlbCYf4WoBdy89RWkNS"
+    }, {
+      "external_urls" : {
+        "spotify" : "https://open.spotify.com/artist/3NKr2YR7MynZKqleKPEvNF"
+      },
+      "followers" : {
+        "href" : null,
+        "total" : 11322
+      },
+      "genres" : [ ],
+      "href" : "https://api.spotify.com/v1/artists/3NKr2YR7MynZKqleKPEvNF",
+      "id" : "3NKr2YR7MynZKqleKPEvNF",
+      "images" : [ {
+        "height" : 640,
+        "url" : "https://i.scdn.co/image/27d088db9624408fef83e2c911548de6e3a526db",
+        "width" : 640
+      }, {
+        "height" : 300,
+        "url" : "https://i.scdn.co/image/976bb8295e69c9dc78b94fdd9a048ef526fb4f51",
+        "width" : 300
+      }, {
+        "height" : 64,
+        "url" : "https://i.scdn.co/image/bb1b269baac91ec23d8603ad9c5eb7d6bc18fa50",
+        "width" : 64
+      } ],
+      "name" : "Iron Man",
+      "popularity" : 43,
+      "type" : "artist",
+      "uri" : "spotify:artist:3NKr2YR7MynZKqleKPEvNF"
+    }, {
+      "external_urls" : {
+        "spotify" : "https://open.spotify.com/artist/2wUYahgemhGsBv9a51NfTn"
+      },
+      "followers" : {
+        "href" : null,
+        "total" : 2063
+      },
+      "genres" : [ ],
+      "href" : "https://api.spotify.com/v1/artists/2wUYahgemhGsBv9a51NfTn",
+      "id" : "2wUYahgemhGsBv9a51NfTn",
+      "images" : [ {
+        "height" : 640,
+        "url" : "https://i.scdn.co/image/c5b75516ded5e370ad901671a09f079105538410",
+        "width" : 640
+      }, {
+        "height" : 300,
+        "url" : "https://i.scdn.co/image/35bfde4ec1a716aaa9d85cc8b52b8607acd4b863",
+        "width" : 300
+      }, {
+        "height" : 64,
+        "url" : "https://i.scdn.co/image/bacd386280d01e65ad63af40f648ec5b25f1cbf0",
+        "width" : 64
+      } ],
+      "name" : "Iron Fire",
+      "popularity" : 35,
+      "type" : "artist",
+      "uri" : "spotify:artist:2wUYahgemhGsBv9a51NfTn"
+    }, {
+      "external_urls" : {
+        "spotify" : "https://open.spotify.com/artist/6G9vhEoxtetSTrcvLee14u"
+      },
+      "followers" : {
+        "href" : null,
+        "total" : 189
+      },
+      "genres" : [ ],
+      "href" : "https://api.spotify.com/v1/artists/6G9vhEoxtetSTrcvLee14u",
+      "id" : "6G9vhEoxtetSTrcvLee14u",
+      "images" : [ {
+        "height" : 640,
+        "url" : "https://i.scdn.co/image/179a7f2b38ee389dcc31cb5c5e11e1eae1b6bd8d",
+        "width" : 640
+      }, {
+        "height" : 300,
+        "url" : "https://i.scdn.co/image/7c9c45366773fa7e2e2d39d9fcfaceecd2e25c25",
+        "width" : 300
+      }, {
+        "height" : 64,
+        "url" : "https://i.scdn.co/image/02f29b228d906416c0430a1425fa056f313c39e3",
+        "width" : 64
+      } ],
+      "name" : "Iron Lamb",
+      "popularity" : 20,
+      "type" : "artist",
+      "uri" : "spotify:artist:6G9vhEoxtetSTrcvLee14u"
+    }, {
+      "external_urls" : {
+        "spotify" : "https://open.spotify.com/artist/160vd9V7P2OiPVOndsKCJO"
+      },
+      "followers" : {
+        "href" : null,
+        "total" : 4140
+      },
+      "genres" : [ ],
+      "href" : "https://api.spotify.com/v1/artists/160vd9V7P2OiPVOndsKCJO",
+      "id" : "160vd9V7P2OiPVOndsKCJO",
+      "images" : [ {
+        "height" : 640,
+        "url" : "https://i.scdn.co/image/a7ac5490c65a1efb262ec7be879457f115bbf204",
+        "width" : 640
+      }, {
+        "height" : 300,
+        "url" : "https://i.scdn.co/image/d31838c38d09306afca43bd99839c0166509cfdd",
+        "width" : 300
+      }, {
+        "height" : 64,
+        "url" : "https://i.scdn.co/image/ce2c387f9fe1d5bccad9d4f587f7ac66d13d14fe",
+        "width" : 64
+      } ],
+      "name" : "Iron Reagan",
+      "popularity" : 42,
+      "type" : "artist",
+      "uri" : "spotify:artist:160vd9V7P2OiPVOndsKCJO"
+    }, {
+      "external_urls" : {
+        "spotify" : "https://open.spotify.com/artist/4fHO1843WfhVrQkRKO555M"
+      },
+      "followers" : {
+        "href" : null,
+        "total" : 1924
+      },
+      "genres" : [ "neo classical metal" ],
+      "href" : "https://api.spotify.com/v1/artists/4fHO1843WfhVrQkRKO555M",
+      "id" : "4fHO1843WfhVrQkRKO555M",
+      "images" : [ {
+        "height" : 640,
+        "url" : "https://i.scdn.co/image/be429cebf1002bddc6f716c9b8b73d59b6eb21e4",
+        "width" : 640
+      }, {
+        "height" : 300,
+        "url" : "https://i.scdn.co/image/7545b1807f159e1ba10ca818ba0c6f35b33a060d",
+        "width" : 300
+      }, {
+        "height" : 64,
+        "url" : "https://i.scdn.co/image/406507ca3f4c027117e5138cb42056ab3a24b0dc",
+        "width" : 64
+      } ],
+      "name" : "Iron Mask",
+      "popularity" : 31,
+      "type" : "artist",
+      "uri" : "spotify:artist:4fHO1843WfhVrQkRKO555M"
+    }, {
+      "external_urls" : {
+        "spotify" : "https://open.spotify.com/artist/1BcAEs4W94BrXMmVCOgwSx"
+      },
+      "followers" : {
+        "href" : null,
+        "total" : 12531
+      },
+      "genres" : [ "christian punk", "ska punk" ],
+      "href" : "https://api.spotify.com/v1/artists/1BcAEs4W94BrXMmVCOgwSx",
+      "id" : "1BcAEs4W94BrXMmVCOgwSx",
+      "images" : [ {
+        "height" : 1233,
+        "url" : "https://i.scdn.co/image/859b921da72c8d48fce1b7f78e8e0be8bb83aff1",
+        "width" : 1000
+      }, {
+        "height" : 789,
+        "url" : "https://i.scdn.co/image/e110acd5042ee129d8755534e0fcee030d64ab37",
+        "width" : 640
+      }, {
+        "height" : 247,
+        "url" : "https://i.scdn.co/image/0012cd4d8e1ac08edcc3c7c03df11c37baeaac32",
+        "width" : 200
+      }, {
+        "height" : 79,
+        "url" : "https://i.scdn.co/image/a5e7dce01b80b297c1cfdbdeacabeb71599e987f",
+        "width" : 64
+      } ],
+      "name" : "Five Iron Frenzy",
+      "popularity" : 48,
+      "type" : "artist",
+      "uri" : "spotify:artist:1BcAEs4W94BrXMmVCOgwSx"
+    }, {
+      "external_urls" : {
+        "spotify" : "https://open.spotify.com/artist/5H32ktDvTByY5e4zJpfFTa"
+      },
+      "followers" : {
+        "href" : null,
+        "total" : 5501
+      },
+      "genres" : [ "orgcore" ],
+      "href" : "https://api.spotify.com/v1/artists/5H32ktDvTByY5e4zJpfFTa",
+      "id" : "5H32ktDvTByY5e4zJpfFTa",
+      "images" : [ {
+        "height" : 652,
+        "url" : "https://i.scdn.co/image/5a2132d95d076edcc2ab251b35fad0d4cdbe5875",
+        "width" : 477
+      }, {
+        "height" : 273,
+        "url" : "https://i.scdn.co/image/0d22bd5c18632593357c59b56698fe728c060c33",
+        "width" : 200
+      }, {
+        "height" : 87,
+        "url" : "https://i.scdn.co/image/b286f5b5a498fdd43dcde5a1fe24aed5484b1337",
+        "width" : 64
+      } ],
+      "name" : "Iron Chic",
+      "popularity" : 45,
+      "type" : "artist",
+      "uri" : "spotify:artist:5H32ktDvTByY5e4zJpfFTa"
+    }, {
+      "external_urls" : {
+        "spotify" : "https://open.spotify.com/artist/5T7eYCcFf7QP2DoIo5gEvF"
+      },
+      "followers" : {
+        "href" : null,
+        "total" : 2138
+      },
+      "genres" : [ ],
+      "href" : "https://api.spotify.com/v1/artists/5T7eYCcFf7QP2DoIo5gEvF",
+      "id" : "5T7eYCcFf7QP2DoIo5gEvF",
+      "images" : [ ],
+      "name" : "Iron And Wine & Calexico",
+      "popularity" : 37,
+      "type" : "artist",
+      "uri" : "spotify:artist:5T7eYCcFf7QP2DoIo5gEvF"
+    }, {
+      "external_urls" : {
+        "spotify" : "https://open.spotify.com/artist/3k0uiQPdZYtPaAGHA5kLBk"
+      },
+      "followers" : {
+        "href" : null,
+        "total" : 216
+      },
+      "genres" : [ ],
+      "href" : "https://api.spotify.com/v1/artists/3k0uiQPdZYtPaAGHA5kLBk",
+      "id" : "3k0uiQPdZYtPaAGHA5kLBk",
+      "images" : [ {
+        "height" : 640,
+        "url" : "https://i.scdn.co/image/541f7f437302fd628fb1de92555a95090812989c",
+        "width" : 640
+      }, {
+        "height" : 300,
+        "url" : "https://i.scdn.co/image/14c61ee098adfaed194de1e99bf02a723e19ec11",
+        "width" : 300
+      }, {
+        "height" : 64,
+        "url" : "https://i.scdn.co/image/48a0f2e3de0f63dcb52aba6027b52b0935e5f4a7",
+        "width" : 64
+      } ],
+      "name" : "Calexico / Iron and Wine",
+      "popularity" : 35,
+      "type" : "artist",
+      "uri" : "spotify:artist:3k0uiQPdZYtPaAGHA5kLBk"
+    }, {
+      "external_urls" : {
+        "spotify" : "https://open.spotify.com/artist/42UIDpEBWHrk9z3rybZQxS"
+      },
+      "followers" : {
+        "href" : null,
+        "total" : 817
+      },
+      "genres" : [ ],
+      "href" : "https://api.spotify.com/v1/artists/42UIDpEBWHrk9z3rybZQxS",
+      "id" : "42UIDpEBWHrk9z3rybZQxS",
+      "images" : [ {
+        "height" : 640,
+        "url" : "https://i.scdn.co/image/7679afa833c5a63ed6dfefaa1936e67d73391c14",
+        "width" : 640
+      }, {
+        "height" : 300,
+        "url" : "https://i.scdn.co/image/a95941a57c81cc5a4d615aa398e832420a623e83",
+        "width" : 300
+      }, {
+        "height" : 64,
+        "url" : "https://i.scdn.co/image/160403de7c14c62b259fdf64acd8d77768ebea91",
+        "width" : 64
+      } ],
+      "name" : "Iron Sy",
+      "popularity" : 32,
+      "type" : "artist",
+      "uri" : "spotify:artist:42UIDpEBWHrk9z3rybZQxS"
+    }, {
+      "external_urls" : {
+        "spotify" : "https://open.spotify.com/artist/6aSkddzlXCNz7WiciWXnFA"
+      },
+      "followers" : {
+        "href" : null,
+        "total" : 474
+      },
+      "genres" : [ "psychedelic blues-rock" ],
+      "href" : "https://api.spotify.com/v1/artists/6aSkddzlXCNz7WiciWXnFA",
+      "id" : "6aSkddzlXCNz7WiciWXnFA",
+      "images" : [ {
+        "height" : 640,
+        "url" : "https://i.scdn.co/image/bbce29cd5ff75b6ace1c0cf0e795ab5fa2f566e3",
+        "width" : 640
+      }, {
+        "height" : 300,
+        "url" : "https://i.scdn.co/image/b2eb813f3cdc34d82ee55ab82d773e3da13387d5",
+        "width" : 300
+      }, {
+        "height" : 64,
+        "url" : "https://i.scdn.co/image/38b4b7022befb53031a78389534a4e2857177ec1",
+        "width" : 64
+      } ],
+      "name" : "Iron Claw",
+      "popularity" : 25,
+      "type" : "artist",
+      "uri" : "spotify:artist:6aSkddzlXCNz7WiciWXnFA"
+    }, {
+      "external_urls" : {
+        "spotify" : "https://open.spotify.com/artist/4FNLr8Xkf9REYFpAjLqDtg"
+      },
+      "followers" : {
+        "href" : null,
+        "total" : 693
+      },
+      "genres" : [ ],
+      "href" : "https://api.spotify.com/v1/artists/4FNLr8Xkf9REYFpAjLqDtg",
+      "id" : "4FNLr8Xkf9REYFpAjLqDtg",
+      "images" : [ {
+        "height" : 640,
+        "url" : "https://i.scdn.co/image/f2fd492201be3514407c3f5dca3eaf5d70776f64",
+        "width" : 640
+      }, {
+        "height" : 300,
+        "url" : "https://i.scdn.co/image/319ef24def4a8b8606e7b47d2ebbcfb1ad423ea4",
+        "width" : 300
+      }, {
+        "height" : 64,
+        "url" : "https://i.scdn.co/image/cb6eef3ad8e31fbbc28140b5c0067427d477a690",
+        "width" : 64
+      } ],
+      "name" : "Iron Weasel",
+      "popularity" : 27,
+      "type" : "artist",
+      "uri" : "spotify:artist:4FNLr8Xkf9REYFpAjLqDtg"
+    }, {
+      "external_urls" : {
+        "spotify" : "https://open.spotify.com/artist/3SxSMKGboN9d8DxDRIB7pH"
+      },
+      "followers" : {
+        "href" : null,
+        "total" : 576
+      },
+      "genres" : [ ],
+      "href" : "https://api.spotify.com/v1/artists/3SxSMKGboN9d8DxDRIB7pH",
+      "id" : "3SxSMKGboN9d8DxDRIB7pH",
+      "images" : [ {
+        "height" : 640,
+        "url" : "https://i.scdn.co/image/01f93873c1035011411e20da14ad0f115b07879d",
+        "width" : 640
+      }, {
+        "height" : 300,
+        "url" : "https://i.scdn.co/image/fafdb120f3698df616211da55355283b082540f4",
+        "width" : 300
+      }, {
+        "height" : 64,
+        "url" : "https://i.scdn.co/image/524f5a98c3455cd128bc931a8c0399288d1b3071",
+        "width" : 64
+      } ],
+      "name" : "Iron Curtis",
+      "popularity" : 24,
+      "type" : "artist",
+      "uri" : "spotify:artist:3SxSMKGboN9d8DxDRIB7pH"
+    }, {
+      "external_urls" : {
+        "spotify" : "https://open.spotify.com/artist/4dE6fQszqtMaD4kdQIqJXc"
+      },
+      "followers" : {
+        "href" : null,
+        "total" : 30
+      },
+      "genres" : [ ],
+      "href" : "https://api.spotify.com/v1/artists/4dE6fQszqtMaD4kdQIqJXc",
+      "id" : "4dE6fQszqtMaD4kdQIqJXc",
+      "images" : [ {
+        "height" : 640,
+        "url" : "https://i.scdn.co/image/0a8ef7d78979dc594fe25bd8d52d53f4aa8bbd29",
+        "width" : 640
+      }, {
+        "height" : 300,
+        "url" : "https://i.scdn.co/image/5010db644c190a4cf21bd72ecca659badec1dc4f",
+        "width" : 300
+      }, {
+        "height" : 64,
+        "url" : "https://i.scdn.co/image/f5d5f5fd58d143667106a11cf00879df4bf16709",
+        "width" : 64
+      } ],
+      "name" : "Iron African",
+      "popularity" : 6,
+      "type" : "artist",
+      "uri" : "spotify:artist:4dE6fQszqtMaD4kdQIqJXc"
+    } ],
+    "limit" : 20,
+    "next" : "https://api.spotify.com/v1/search?query=iron&offset=20&limit=20&type=artist",
+    "offset" : 0,
+    "previous" : null,
+    "total" : 369
+  }
+}
--- main_search.js
+++ main_search.js
@@ -6,10 +6,10 @@ function fetchArtists (event) {
     console.log(artists);
     artists.artists.items.forEach(function showArtists(artist_searched){
       console.log(artist_searched.name);
-      var name_image = '<li><h3>'+ artist_searched.name + '<h3><li><img src ="' + artist_searched.images[0].url + '">';
+      var name_image = '<li><h3>'+ '<button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">'
+  + artist_searched.name + '</button><h3><li><img src ="' + artist_searched.images[0].url + '">';
       $('#showroom').append(name_image);
     })
-
   }
 
   function handleError (err) {
@@ -20,4 +20,26 @@ function fetchArtists (event) {
   request.fail(handleError);
 }
 
+function fetchAlbums (event) {
+  event.preventDefault()
+  var request = $.get('https://api.spotify.com/v1/search?q=' + $(".btn-primary").text() + '&type=album');
+    
+  function handleAlbums (albums) {
+    console.log(albums);
+    albums.albums.items.forEach(function showAlbums(albums_searched){
+      console.log(albums_searched.name);
+      var name_image = '<li><h3>' + albums_searched.name + '<h3><li><img height="64" width="64" src ="' + albums_searched.images[0].url + '">';
+      $('.modal-body').append(name_image);
+    })
+    $('.modal-title').append($(".btn-primary").text());
+  }
+
+  function handleAlbumError (err) {
+    console.error('OH NO!!', err);
+  }
+  request.done(handleAlbums);
+  request.fail(handleAlbumError);
+}
+
 $('#js-submit').on('click', fetchArtists);
+$('#myModal').on('shown.bs.modal', fetchAlbums);
